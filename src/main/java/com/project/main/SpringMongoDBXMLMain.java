0590dd72046271fc77eb36e813e02fe8800dbe73
apivakim <nuva_apiva@outlook.co.th>
2016-06-16 15:48:36 +0700

    edit gitignore and SpringMongoDBXMLMain.java

--- src/main/java/com/project/main/SpringMongoDBXMLMain.java
+++ src/main/java/com/project/main/SpringMongoDBXMLMain.java
@@ -24,14 +24,14 @@ public class SpringMongoDBXMLMain {
         System.out.println("Retrieved Person="+p1);
 
         //update
-        /*p1.setName("David");p1.setAddress("SFO, USA");
+        p1.setName("David");p1.setAddress("SFO, USA");
         personDAO.update(p1);
         Person temp = personDAO.readById(p1.getId());
         System.out.println("Retrieved Person after update="+temp);
 
         //delete
         int count = personDAO.deleteById(p1.getId());
-        System.out.println("Number of records deleted="+count);*/
+        System.out.println("Number of records deleted="+count);
 
         ctx.close();
 
